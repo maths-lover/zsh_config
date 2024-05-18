b376b4b97e9a0b6839519cb352fff685e05f49f0
Suraj Pal Singh <dev.suraj.pal@gmail.com>
2024-05-18 16:17:41 +0530

    Add: poetry module

---
+++ modules/poetry/init.zsh
@@ -0,0 +1,13 @@
+autoload -Uz compinit
+_comp_path="${XDG_CACHE_HOME:-$HOME/.cache}/prezto/zcompdump"
+# #q expands globs in conditional expressions
+if [[ $_comp_path(#qNmh-20) ]]; then
+  # -C (skip function check) implies -i (skip security check).
+  compinit -C -d "$_comp_path"
+else
+  mkdir -p "$_comp_path:h"
+  compinit -i -d "$_comp_path"
+  # Keep $_comp_path younger than cache time even if it isn't regenerated.
+  touch "$_comp_path"
+fi
+unset _comp_path
