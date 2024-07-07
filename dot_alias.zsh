b40afbce394f53c5d54c17c8d22cbeadbaeef0ef
Suraj Pal Singh <dev.suraj.pal@gmail.com>
2024-07-07 12:44:56 +0530

    Update: Have more dynamic config with variables

---
+++ dot_alias.zsh
@@ -0,0 +1,6 @@
+alias cd="z"
+
+alias wifi_status="iwctl station wlan0 get-networks"
+alias wifi_connect="iwctl station wlan0 connect"
+alias wifi_disconnect="iwctl station wlan0 disconnect"
+alias wifi_scan="iwctl station wlan0 scan"
